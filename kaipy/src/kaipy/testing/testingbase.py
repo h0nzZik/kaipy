b2fbf576b2f12e86fefbd6886742910a7933b85b
Jan Tusil <jenda.tusil@gmail.com>
2023-08-03 15:02:58 +0200

    forgotten file

---
+++ kaipy/src/kaipy/testing/testingbase.py
@@ -0,0 +1,30 @@
+import typing as T
+from pathlib import Path
+
+import pytest
+from pyk.testing._kompiler import KompiledTest
+
+from kaipy.KompiledDefinitionWrapper import KompiledDefinitionWrapper
+from kaipy.ReachabilitySystem import ReachabilitySystem
+
+
+class RSTestBase(KompiledTest):
+    LANGUAGES: T.Final = (
+        Path(__file__).parent.parent.parent.parent
+        / "tests"
+        / "integration"
+        / "languages"
+    ).resolve(strict=True)
+
+    @pytest.fixture
+    def kompiled_definition_wrapper(
+        self, definition_dir: Path
+    ) -> KompiledDefinitionWrapper:
+        return KompiledDefinitionWrapper.load_from_dir(definition_dir)
+
+    @pytest.fixture
+    def reachability_system(
+        self, kompiled_definition_wrapper: KompiledDefinitionWrapper
+    ) -> T.Iterator[ReachabilitySystem]:
+        with ReachabilitySystem(kdw=kompiled_definition_wrapper) as rs:
+            yield rs
