8eb81d1784029978b73a4b716377e64921235eed
Jan Tusil <jenda.tusil@gmail.com>
2023-08-31 11:22:04 +0200

    BigsumPatternDomain

--- kaipy/src/kaipy/domains/BigsumPatternDomain.py
+++ kaipy/src/kaipy/domains/BigsumPatternDomain.py
@@ -4,7 +4,8 @@ import typing as T
 import pyk.kore.syntax as Kore
 
 from kaipy.ReachabilitySystem import ReachabilitySystem
-from kaipy.IAbstractPatternDomain import IAbstractPattern, IAbstractPatternDomain
+from kaipy.AbstractionContext import AbstractionContext
+from kaipy.interfaces.IAbstractPatternDomain import IAbstractPattern, IAbstractPatternDomain
 
 @dataclasses.dataclass
 class BigsumPattern(IAbstractPattern):
@@ -23,14 +24,22 @@ class BigsumPatternDomain(IAbstractPatternDomain):
         self.rs = rs
         self.domains = domains
     
-    def abstract(self, c: Kore.Pattern) -> BigsumPattern:
+    def abstract(self, ctx: AbstractionContext, c: Kore.Pattern) -> BigsumPattern:
         sort = self.rs.kdw.sortof(c)
         for i,d in enumerate(self.domains):
-            a = d.abstract(c)
+            a = d.abstract(ctx, c)
             if not d.is_top(a):
                 return BigsumPattern(idx=i, sort=sort, ap=a)
         return BigsumPattern(-1, sort=sort, ap=None)            
     
+
+    def disjunction(self, ctx: AbstractionContext, a1: IAbstractPattern, a2: IAbstractPattern) -> BigsumPattern:
+        assert type(a1) is BigsumPattern
+        assert type(a2) is BigsumPattern
+        if a1.idx == a2.idx and a1.sort == a2.sort and a1.ap is not None and a2.ap is not None:
+            return BigsumPattern(idx=a1.idx, sort=a1.sort, ap=self.domains[a1.idx].disjunction(ctx, a1.ap, a2.ap))
+        return BigsumPattern(-1, sort=a1.sort, ap=None)
+
     def is_top(self, a: IAbstractPattern) -> bool:
         assert type(a) is BigsumPattern
         return (a.idx == -1) or (a.ap is None)
@@ -81,8 +90,8 @@ class BigsumPatternDomain(IAbstractPatternDomain):
 
         
 
-    def print(self, a: IAbstractPattern) -> str:
+    def to_str(self, a: IAbstractPattern) -> str:
         assert type(a) is BigsumPattern
         if (a.idx < 0) or (a.ap is None):
             return f'<bigsum Top>'
-        return f'<bigsum idx={a.idx}, a={self.domains[a.idx].print(a.ap)}>'
+        return f'<bigsum idx={a.idx}, a={self.domains[a.idx].to_str(a.ap)}>'
